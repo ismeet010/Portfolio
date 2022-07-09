5ef03775dbc022b61b2b55003df8c1af4e878704
ismeet010 <ismeetchhabda@gmail.com>
2022-07-10 02:54:35 +0530

    added bouncing effect to letters

---
+++ bounce.js
@@ -0,0 +1,17 @@
+  /*bouncing letters*/
+  document.querySelectorAll(".bouncing-letters>span").forEach((element) => {
+    element.addEventListener("mouseover", (e) => bounce(e.target));
+});
+
+function bounce(letter) {
+    if (!letter.classList.contains("bounce")) {
+        letter.classList.add("bounce");
+        setTimeout(
+            function () {
+                letter.classList.remove("bounce");
+            },
+            1000
+        );
+    }
+}
+
