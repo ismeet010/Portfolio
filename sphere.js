7b23601367f5306258162c2fd59097bd4ee61863
ismeet010 <ismeetchhabda@gmail.com>
2022-08-07 01:31:49 +0530

    Form Validation

--- sphere.js
+++ sphere.js
@@ -26,3 +26,44 @@ var tagCloud = TagCloud('.Sphere', Texts, {
 // Giving color to each text in sphere
 var color = '#FF5733';
 document.querySelector('.Sphere').style.color = color;
+
+//Contact Form Validation
+function Validation() {
+				var name =
+					document.forms.ContactForm.Name.value;
+				var email =
+					document.forms.ContactForm.EMail.value;
+				var what =
+					document.forms.ContactForm.Subject.value;
+				var message =
+					document.forms.ContactForm.Message.value;
+				var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g; //Javascript reGex for Email Validation.
+				var regName = /\d+$/g;								 // Javascript reGex for Name validation
+
+        if (name == "" || regName.test(name)) {
+          document.getElementById("name").classList.add("below");
+                  return false;
+                }else{
+                    document.getElementById("name").classList.remove("below");
+                  }
+          
+                if (email == "" || !regEmail.test(email)) {
+                  document.getElementById("email").classList.add("below");
+                  return false;
+                }else{
+                  document.getElementById("email").classList.remove("below");
+                }
+        
+                if (what == "") {
+                  var element = document.getElementById("sub");
+                  element.classList.add("below");
+                  return false;
+                }
+        
+                if (message == "") {
+                  var element = document.getElementById("msg");
+                  element.classList.add("below");
+                  return false;
+                }
+                return true;
+              }
